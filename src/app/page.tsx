a92b93f98daf30e0df3de36dd67d534179a1c47c
RaymondBello <raymondbello11@gmail.com>
2024-04-27 13:55:26 -0400

    Added initial UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,87 @@
+import Image from "next/image";
 import Link from "next/link";
+
+const mockItemImageUrls = [
+  "https://utfs.io/f/e3e1f593-33a8-48a2-8b0d-ccb852e5386f-1xcy6.jpeg",
+  "https://utfs.io/f/7870cb49-2506-45e2-b90d-f3ba2e4e4046-1xcy7.jpeg",
+  "https://utfs.io/f/264ba535-0351-4431-bfa9-1a8643d0884e-1xcy8.jpeg",
+  "https://utfs.io/f/83883937-bebe-4fe5-8281-34d03fd27038-1xcy9.jpeg",
+];
+
+const mockItems = mockItemImageUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+    <main className="">
+      <div className="flex flex-wrap space-x-4">
+        {[...mockItems, ...mockItems].map((item) => (
+          <div key={item.id} className="mb-4 rounded border p-4 ">
+            <div className="mb-2 flex items-center justify-between">
+              <h2 className="text-lg font-bold"> Item {item.id} </h2>
+              <button className="text-sm text-red-500 hover:text-red-700">
+                <p> Delete </p>
+              </button>
             </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
+
+            <div className="mb-4">
+              <div className="flex flex-col space-y-2">
+                <div className="flex justify-between space-x-2 text-gray-600">
+                  <p>Subitem1</p>
+                  <button className="text-red-500 hover:text-red-700">
+                    <p> X </p>
+                  </button>
+                </div>
+                <div className="flex justify-between space-x-2 text-gray-600">
+                  <p>Subitem2</p>
+                  <button className="text-red-500 hover:text-red-700">
+                    <p> X </p>
+                  </button>
+                </div>
+                <div className="flex justify-between space-x-2 text-gray-600">
+                  <p>Subitem3</p>
+                  <button className="text-red-500 hover:text-red-700">
+                    <p> X </p>
+                  </button>
+                </div>
+                <button className="rounded bg-blue-500 px-2 text-white hover:bg-blue-700 ">
+                  + Add Subitem
+                </button>
+              </div>
+              <hr className="my-2"></hr>
+              <div className="flex flex-col space-y-2">
+                <div className="flex justify-between space-x-2 text-gray-600">
+                  <p>Link 1</p>
+                  <button className="text-red-500 hover:text-red-700">
+                    <p> X </p>
+                  </button>
+                </div>
+                <button className="rounded bg-blue-500 px-2 text-white hover:bg-blue-700 ">
+                  + Add Link
+                </button>
+              </div>
             </div>
-          </Link>
-        </div>
+            <Image
+              src={item.url}
+              alt={`Item ${item.id}`}
+              className="mb-4 w-full rounded-lg"
+              width={50}
+              height={50}
+            />
+            <div className="flex flex-col py-2">
+              <div className="text-gray-500">Start date: 01/01/2022</div>
+              <div className="text-gray-500">End date: 01/02/2022</div>
+            </div>
+            <div className="flex flex-col space-y-2 py-2">
+              <div className="text-gray-500">Status: In-Progress</div>
+              <div className="text-gray-500">Tags: Flight, Physics</div>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
 }
+
